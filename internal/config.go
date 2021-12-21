0c5de53e2189fd9a688cb026151d6676b7f07e77
Geert-Johan Riemer <geertjohan@geertjohan.net>
2021-12-21 11:30:03 +0100

    Validate identifiers in the trek.yaml config file

--- internal/config.go
+++ internal/config.go
@@ -1,11 +1,17 @@
 package internal
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"regexp"
 
 	"gopkg.in/yaml.v2"
 )
+
+const regexpStringValidIdentifier = `^[a-z_]+$`
+
+var ErrInvalidValuesInConfig = errors.New("invalid values in config")
 
 type Config struct {
 	//nolint:tagliatelle
@@ -27,5 +33,43 @@ func ReadConfig() (*Config, error) {
 		return nil, fmt.Errorf("failed to parse config file: %w", err)
 	}
 
+	problems := config.validate()
+	if len(problems) > 0 {
+		for _, problem := range problems {
+			fmt.Println("Error in trek.yaml: " + problem)
+		}
+
+		return nil, ErrInvalidValuesInConfig
+	}
+
 	return config, nil
+}
+
+func (c *Config) validate() (problems []string) {
+	var regexpValidIdentifier = regexp.MustCompile(regexpStringValidIdentifier)
+	if !regexpValidIdentifier.MatchString(c.ModelName) {
+		p := fmt.Sprintf("Model name %q contains invalid characters. Must match %q.",
+			c.ModelName,
+			regexpStringValidIdentifier,
+		)
+		problems = append(problems, p)
+	}
+	if !regexpValidIdentifier.MatchString(c.DatabaseName) {
+		p := fmt.Sprintf("Database name %q contains invalid characters. Must match %q.",
+			c.DatabaseName,
+			regexpStringValidIdentifier,
+		)
+		problems = append(problems, p)
+	}
+	for _, user := range c.DatabaseUsers {
+		if !regexpValidIdentifier.MatchString(user) {
+			p := fmt.Sprintf("Database user %q contains invalid characters. Must match %q.",
+				user,
+				regexpStringValidIdentifier,
+			)
+			problems = append(problems, p)
+		}
+	}
+
+	return problems
 }
